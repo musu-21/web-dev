1eb783040d612f2583f9bd514e244766dbbbeb2c
musu-21 <taqiyuk1974@gmail.com>
2026-05-25 11:30:53 +0700

    Hari2BelajarVariable

---
+++ Hari2/notes.md
@@ -0,0 +1,13 @@
+Hari 2 =
+- variable txt memiliki nilai hello word
+- echo memerintahkan untuk mencetak woy ditambah variable txt dan juga kuk
+
+- variable x memiliki nilai 9, variable y memiliki nilai 73
+- echo memerintahkan untuk menghasilkan perkalian dari kedua  variable tersebut
+
+- TIPE DATA
+    - echo memberi perintah untuk mencetak variable x serta y
+
+- fungsi var_dump untuk mendapatkan tipe data dan nilai variable
+
+- ketiga variable x, y, z memiliki nilai yang sama yakni 99
---
+++ Hari2/variable.php
@@ -0,0 +1,27 @@
+<?php
+/*$txt = "Hello Word";
+echo 'woy ' . $txt . ' kuk';*/
+
+/* PERHITUNGAN */
+/*$x = 7;
+$y = 73;
+
+echo $x * $y;*/
+
+/* TIPE DATA */
+
+/* $x = 15; // tipe data dari nilai variable x ini ialah integer
+$y = "mahasiwa"; // tipe data dari nilai variable y ini ialah string
+echo $x;
+echo $y; */
+
+/* var_dump(5); // tipe data integer
+var_dump("John"); // tipe data string
+var_dump(3.14); // tipe data float
+var_dump(true); // tipe data boolean
+var_dump([2, 3, 56]); // tipe data array
+var_dump(NULL); */
+
+$x = $y = $z = 99;
+echo $y;
+?>
